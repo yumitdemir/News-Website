a38b5b0d41eca74bb8e48f327cc9efdf641b78b0
Yumit Demir <yumitdemir@outlook.com>
2023-04-02 16:59:07 +0300

    Initial push

---
+++ DSS/Program.cs
@@ -0,0 +1,48 @@
+
+namespace DSS
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+            app.MapControllerRoute(
+                name: "SignInRoute",
+                pattern: "Login/SignIn",
+                defaults: new { controller = "Login", action = "SignIn" });
+
+            app.MapControllerRoute(
+                name: "SignUpRoute",
+                pattern: "Login/SignUp",
+                defaults: new { controller = "Login", action = "SignUp" });
+
+
+
+            app.Run();
+        }
+    }
+}
