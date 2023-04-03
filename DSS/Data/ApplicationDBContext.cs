98299e6673a2215b66792fcd8034296514ae502c
Yumit Demir <yumitdemir@outlook.com>
2023-04-03 12:58:13 +0300

    add user model migration

---
+++ DSS/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using DSS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSS.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        
+
+    }
+}
--- DSS/Program.cs
+++ DSS/Program.cs
@@ -1,62 +1,62 @@
-
+using DSS.Data;
 using DSS.Models;
 using DSS.Sessions;
+using Microsoft.EntityFrameworkCore;
 
-namespace DSS
+namespace DSS;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
+        var builder = WebApplication.CreateBuilder(args);
 
-        public static void Main(string[] args)
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddSession(); //! Related to authentication with session
+        builder.Services.AddScoped<IAccountService, AccountService>(); //! Related to authentication with session
+
+
+        builder.Services.AddDbContext<ApplicationDBContext>(options => //! db context service
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+            options.UseSqlServer(connString);
+        });
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddSession(); //! Related to authentication with session
-            builder.Services.AddScoped<IAccountService, AccountService>(); //! Related to authentication with session
+        var app = builder.Build();
 
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment()) app.UseExceptionHandler("/Home/Error");
+        app.UseStaticFiles();
 
-            var app = builder.Build();
+        app.UseRouting();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-            
-
-            app.UseSession(); //! Related to authentication with session
+        app.UseAuthorization();
 
 
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.UseSession(); //! Related to authentication with session
 
 
+        app.MapControllerRoute(
+            "default",
+            "{controller=Home}/{action=Index}/{id?}");
 
-            app.MapControllerRoute(
-                name: "SignInRoute",
-                pattern: "Account/SignIn",
-                defaults: new { controller = "Account", action = "SignIn" });
 
-            app.MapControllerRoute(
-                name: "SignUpRoute",
-                pattern: "Account/SignUp",
-                defaults: new { controller = "Account", action = "SignUp" });
+        app.MapControllerRoute(
+            "SignInRoute",
+            "Account/SignIn",
+            new { controller = "Account", action = "SignIn" });
 
-            app.MapControllerRoute(
-                name: "DetailsRouting",
-                pattern: "Details/{id}",
-                defaults: new { controller = "Details", action = "Index" });
+        app.MapControllerRoute(
+            "SignUpRoute",
+            "Account/SignUp",
+            new { controller = "Account", action = "SignUp" });
 
-            app.Run();
-        }
+        app.MapControllerRoute(
+            "DetailsRouting",
+            "Details/{id}",
+            new { controller = "Details", action = "Index" });
+
+        app.Run();
     }
 }
